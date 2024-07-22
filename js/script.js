af7ff475b2d7892e2ae47101077efaac556e7603
HL <hoanglinh171038@gmail.com>
2024-07-22 08:18:19 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('myButton');
+const result = document.getElementById('result');
+
+button.addEventListener('click', () => {
+    result.textContent = "Linh";
+});

9b7d2c073a6e02c1e8d59252900fe472c9779367
HL <hoanglinh171038@gmail.com>
2024-07-23 17:12:56 +0700

    5.header component

--- js/script.js
+++ js/script.js
@@ -1,6 +1,27 @@
-const button = document.getElementById('myButton');
-const result = document.getElementById('result');
+// Lấy nội dung của header.html và chèn vào #app
+fetch('components/header.html')
+  .then(response => response.text())
+  .then(data => {
+      const app = document.getElementById('navbar');
+      app.innerHTML = data;
+  })
+  .catch(error => console.error('Lỗi:', error));
+ 
+fetch('components/sidebar/sidebar.html')
+  .then(response => response.text())
+  .then(data => {
+      const app = document.getElementById('sidebar');
+      app.innerHTML = data;
+  })
+  .catch(error => console.error('Lỗi:', error));
 
-button.addEventListener('click', () => {
-    result.textContent = "Linh";
-});
+   
+fetch('components/banner.html')
+.then(response => response.text())
+.then(data => {
+    const app = document.getElementById('banner');
+    app.innerHTML = data;
+})
+.catch(error => console.error('Lỗi:', error));
+
+ 
